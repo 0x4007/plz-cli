067bf8e8de8356f04bfc515db2c8dcc600520d56
アレクサンダー.eth <gpg@pavlovcik.com>
2024-10-03 18:48:53 +0900

    feat/claude-3.5-sonnet

--- src/config.rs
+++ src/config.rs
@@ -9,11 +9,11 @@ pub struct Config {
 
 impl Config {
     pub fn new() -> Self {
-        let api_key = env::var("OPENAI_API_KEY").unwrap_or_else(|_| {
-            println!("{}", "This program requires an OpenAI API key to run. Please set the OPENAI_API_KEY environment variable. https://github.com/m1guelpf/plz-cli#usage".red());
+        let api_key = env::var("ANTHROPIC_API_KEY").unwrap_or_else(|_| {
+            println!("{}", "This program requires an Anthropic API key to run. Please set the ANTHROPIC_API_KEY environment variable.".red());
             exit(1);
         });
-        let api_base = env::var("OPENAI_API_BASE").unwrap_or_else(|_| String::from("https://api.openai.com/v1"));
+        let api_base = env::var("ANTHROPIC_API_BASE").unwrap_or_else(|_| String::from("https://api.anthropic.com"));
         let shell = env::var("SHELL").unwrap_or_else(|_| String::new());
 
         Self { api_key, api_base, shell }
